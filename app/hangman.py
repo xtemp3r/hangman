7b7bece946bb5b089331a2949a2286dc4ad0ec7b
xt3r <adrijan.furlanic@gobjidvor.si>
2021-04-12 19:26:40 +0200

    added dockerfile basics and a working app.py

---
+++ app/hangman.py
@@ -0,0 +1,17 @@
+import sys
+import os
+from colorama import Fore, Back #colored text and background
+
+#global variables
+banner = '''
+  _    _                                         
+ | |  | |                                        
+ | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
+ |  __  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+ | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+ |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+					  __/ |                      
+					 |___/                       
+'''
+
+print(Fore.BLACK + Back.GREEN + banner)
